9b23484d7e0a85c84441a298ae395aa1ddb8a448
Aldo Gallegos <pedroaldo14@gmail.com>
2023-06-21 15:34:32 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/testeando-desde-feature', function () {
+Route::get('/testeando-feature', function () {
     return view('welcome');
 });
 
